f2f30466b4aad6b2a9f7c6d6f914e84be41f4566
Ysrael machado <86720970097@ulife.com.br>
2026-07-28 15:39:12 -0300

    chore: add lint and formatting tooling

---
+++ eslint.config.js
@@ -0,0 +1,28 @@
+import js from '@eslint/js';
+import globals from 'globals';
+
+export default [
+  {
+    ignores: ['dist/**', 'node_modules/**'],
+  },
+  js.configs.recommended,
+  {
+    files: ['src/**/*.js'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: globals.browser,
+    },
+  },
+  {
+    files: ['src/__tests__/**/*.js', '*.config.js', 'eslint.config.js'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
+];
